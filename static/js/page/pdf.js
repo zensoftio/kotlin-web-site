a33314cacec3b3f9efc586291111e35db1ba329f
Prendota <Prendota@mail.ru>
2018-11-29 18:41:22 +0300

    chore(pdf): ппп

--- static/js/page/pdf.js
+++ static/js/page/pdf.js
@@ -13,14 +13,14 @@ $(document).ready(function () {
   $('.sample').each((ind, element) => {
     const codeElement = $(element).find('code')[0];
     let code = codeElement.textContent;
-    code = code.split('\n').filter(line => !line.includes(SAMPLE_END)).filter(line => !line.includes(SAMPLE_START)).join("\n")
-    // const startIndex = code.indexOf(SAMPLE_START);
-    // const endIndex = code.indexOf(SAMPLE_END);
-    // if (startIndex > -1 && endIndex > -1) {
-    //   code = code.substring(code.indexOf(SAMPLE_START) + SAMPLE_START.length + 1);
-    //   code = code.substring(0, code.indexOf(SAMPLE_END));
-    //   if (code.charAt(0) === " ") code = code.split('\n').map(l => l.substring(2)).join("\n")
-    // }
+    const startIndex = code.indexOf(SAMPLE_START);
+    const endIndex = code.indexOf(SAMPLE_END);
+    if (startIndex > -1 && endIndex > -1) {
+      code = code.replace(`${SAMPLE_END}\n`,'').replace(`${SAMPLE_START}\n`,'');
+      // code = code.substring(code.indexOf(SAMPLE_START) + SAMPLE_START.length + 1);
+      // code = code.substring(0, code.indexOf(SAMPLE_END));
+      // if (code.charAt(0) === " ") code = code.split('\n').map(l => l.substring(2)).join("\n")
+    }
     codeElement.textContent = code;
     hljs.highlightBlock(codeElement);
   });
